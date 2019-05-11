c8ee020ee9cef3b2377e56176608bcff98d55a31
afestradam <afestradam@gmail.com>
2019-05-11 12:01:20 -0500

    n
    mlmlk

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,8 @@ module.exports = function(grunt) {
   grunt.initConfig({
     'create-windows-installer': {
       ia32: {
-        appDirectory: './MyApp-win32-ia32',
-        outputDirectory: './dist',
+        appDirectory: './build/shakeitcol-32',
+        outputDirectory: './dist/installer32',
         name: 'shakeitcol',
         description: 'Aplicaión de escritorio para facturación y cotrol de sedes.',
         authors: 'Softan Soluciones',
